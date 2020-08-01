b9c68f5411dc7ad21b42b88e7d8f93224820eba6
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2020-08-01 01:56:03 -0600

    Load a spec

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type Spec map[string]interface{}
+
 func main() {
+	s := loadSpec("bundle/7.0/spec.json")
+	fmt.Println(s)
+}
+
+func loadSpec(file string) (s Spec) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := json.Unmarshal(data, &s); err != nil {
+		log.Fatalln(err)
+	}
+	return
 }
